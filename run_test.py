5d30e5b72e8be79b047fcfdf174232f35c9d41d6
NancySun75 <honeyhoneystar@126.com>
2018-05-08 13:11:56 +0800

    Add htmltestRunner

--- run_test.py
+++ run_test.py
@@ -1,10 +1,15 @@
 """Run all test."""
 from test_case.CreateAsmtTest import CreateAsmtTest
 import unittest
+from HTMLTestRunner import HTMLTestRunner
 
 
 suite = unittest.TestSuite()
 suite.addTest(CreateAsmtTest("test_gp_asmt_create"))
 
-runner = unittest.TextTestRunner()
+fp = open('./result.html', 'wb')
+runner = HTMLTestRunner(stream = fp, title = 'create gp test report', description = 'test case execute result: ')
+#runner = unittest.TextTestRunner()
 runner.run(suite)
+fp.close()
+
